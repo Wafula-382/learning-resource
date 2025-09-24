cadc8305ab4eeb5c0ca00c757f4d88fe644c442c
Wafula-382 <gabwafula382@gmail.com>
2025-09-24 14:01:53 +0300

    Add files via upload

---
+++ Wafula1A.py
@@ -0,0 +1,8 @@
+"Bible Studies" 
+import {"bible_studies"} from [readings].{bible_studies}
+
+print{king james version (bible)} 
+extract{bible_studies}
+
+bible readings = {"Genesis 1:1-5", "John 1:1-5", "Psalm 23"}
+BIBLE_VERSION = "KJV"
