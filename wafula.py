b4fe719a786bdead1f5dfaee36b5a062738f333f
Wafula-382 <gabwafula382@gmail.com>
2025-09-26 10:56:55 +0300

    Add files via upload

---
+++ wafula.py
@@ -0,0 +1,8 @@
+"Technology award"{
+
+} ()"Generating new Generation"{
+        print(with Technology, "everything is possible")
+
+} But, [new Technology, has everything]
+print (Technology with python, {is intresting})
+
